3bb84e6de07a75879a59665277ee05a90022d933
Vasyalama <137906229+Vasyalama@users.noreply.github.com>
2024-03-13 07:35:50 +0000

    did task3-1

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
-/* hello.c */
 #include <stdio.h>
-
-void main (void)
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char *argv[])
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  if(argc == 2){
+    char ch;
+    int i;
+    printf("Original string: %s\n", argv[1]);
+    printf("The frequency of all characters in the string are : \n");
+    for(i=0; i<=strlen(argv[1]); i++)
+    {
+      ch = *(argv[1]+i);
+      printf("%c%d",ch,ch);	
+    }
+    printf("\n\n");
+    return 0;
+  }
+  printf("Invalid arguments \n");
+  return 0;
 }
